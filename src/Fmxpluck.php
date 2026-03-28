2f9264d65d2db1a1e8f4d37af76737a220170ac7
Enrique Zermeño <jerryhenry75@gmail.com>
2026-03-28 01:11:42 -0600

    Update versión 2.0.1 add option to use relations "table.fields" in array of fields to get

--- src/Fmxpluck.php
+++ src/Fmxpluck.php
@@ -7,47 +7,44 @@ trait Fmxpluck
 {
 
    
-   public function scopeFmxPluck($query, $columnas = [], $id = null, $separador = " ")
+  public function scopeFmxPluck($query, $columnas = [], $id = null, $separador = " ")
    {
        $selects = [];
        $joins   = [];
-   
+  
        foreach ($columnas as $col) {
+  
            if (strpos($col, '.') !== false) {
+  
+  
                [$relacion, $campo] = explode('.', $col);
-   
-               // Si el primer segmento ya es el nombre de la tabla (ej: "puestos.nombre")
-              
-               if (Str::plural($relacion) === $relacion || $relacion === $query->getModel()->getTable()) {
-                   $selects[] = $relacion.'.'.$campo;
-               } else {
-                   // Caso: relación tipo belongsTo (ej: "puesto.nombre")
-                   $tablaRelacion = Str::plural($relacion); // "puesto" → "puestos"
-                   $fk = $relacion.'_id';
-   
-                   $joins[$tablaRelacion] = [$tablaRelacion.'.id', '=', $query->getModel()->getTable().'.'.$fk];
-                   $selects[] = $tablaRelacion.'.'.$campo;
-               }
-           } else {
+  
+                $tablaRelacion = $relacion;
+                $fk = $relacion.'_id';
+  
+                $joins[$tablaRelacion] = [$tablaRelacion.'.id', '=', $query->getModel()->getTable().'.'.$fk];
+  
+                $selects[] = $tablaRelacion.'.'.$campo;
+  
+            } else {
                $selects[] = $query->getModel()->getTable().'.'.$col;
-           }
+            }
+  
        }
-   
-       // Agregar los joins
+     
        foreach ($joins as $tabla => $on) {
            $query->leftJoin($tabla, $on[0], $on[1], $on[2]);
        }
-   
-       // Concatenar columnas
+       
        $campos = implode(', " '.$separador.' ", ', $selects);
-   
-       $registros = $query->selectRaw('CONCAT('.$campos.') as opcion, '.$id)->get();
-   
+  
+       $registros = $query->selectRaw('CONCAT('.$campos.') as opcion, '. $query->getModel()->getTable().'.'.$id)->get();
+  
        $aArray = [];
        foreach ($registros as $registro) {
            $aArray[$registro->$id] = $registro->opcion;
        }
-   
+  
        return $aArray;
    }
 
